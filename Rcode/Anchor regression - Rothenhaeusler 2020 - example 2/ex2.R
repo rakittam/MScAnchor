68912e31b5bc29c442a07d7640fe7bf92af3175b
rakittam <rakittam@student.ethz.ch>
2020-09-23 14:04:34 +0200

    some embellishments, true optimal gamma

--- Rcode/Anchor regression - Rothenhaeusler 2020 - example 2/ex2.R
+++ Rcode/Anchor regression - Rothenhaeusler 2020 - example 2/ex2.R
@@ -4,11 +4,10 @@
 ##########################################################################
 set.seed(1)
 
-n <- 1000
-
-library(extraDistr)
+n <- 1000 # number of samples from unpertubed and pertubed distribution
 
 # initialize training data
+library(extraDistr) # for rademacher distribution
 A <- rsign(n)
 epsH.train <- rnorm(n)
 epsX.train <- rnorm(n)
@@ -28,8 +27,7 @@ X.test <- 1.8+H.test+epsX.test
 Y.test <- X.test+2*H.test+epsY.test
 
 ##########################################################################
-#Anchor regression
-
+# Anchor regression
 anchor.regression <- function(X, Y, A, gamma, n){
   
   P.A <- A%*%solve(t(A)%*%A)%*%t(A)
@@ -38,22 +36,18 @@ anchor.regression <- function(X, Y, A, gamma, n){
   Y.tilde <- W%*%Y
   X.tilde <- W%*%X
   
-  fit.AR <- lm(Y.tilde~X.tilde-1)
+  fit <- lm(Y.tilde~X.tilde-1)
 }
 
-fit.AR <- anchor.regression(X.train, Y.train, A, 2, n)
-summary(fit.AR)
-
-
-#PA
+# PA = AR for gamma = 0
 fit.PA <- anchor.regression(X.train, Y.train, A, 0, n)
 b.PA <- coef(fit.PA)
 
-#OLS
+# OLS = AR for gamma = 1
 fit.OLS <- anchor.regression(X.train, Y.train, A, 1, n)
 b.OLS <- coef(fit.OLS)
 
-#IV
+# IV - 2SLS
 fit.2SLS.step <- lm(X.train~A-1)
 X.train.hat <- fitted.values(fit.2SLS.step)
 fit.IV <- lm(Y.train~X.train.hat-1)
@@ -63,7 +57,7 @@ fit.IV <- lm(Y.train~X.train.hat-1)
 #b.IV <- solve(t(X.train.tilde)%*%X.train.tilde)%*%t(X.train.tilde)%*%Y.train
 b.IV <- coef(fit.IV)
 
-#training data fit
+# training data plot
 plot(X.train,Y.train)
 abline(fit.OLS, col="2")
 abline(fit.IV, col="3")
@@ -71,7 +65,7 @@ abline(fit.PA,col="4")
 legend(0, -5, legend=c("OLS", "IV", "PA"),
        col=c(2, 3,4), lty=1, cex=0.8)
 
-#test data fit
+# test data plot
 plot(X.test,Y.test)
 abline(fit.OLS, col="2")
 abline(fit.IV, col="3")
@@ -79,13 +73,8 @@ abline(fit.PA,col="4")
 legend(2, -3, legend=c("OLS", "IV", "PA"),
        col=c(2, 3,4), lty=1, cex=0.8)
 
-# Training MSE
-MSE.PA <- mean((Y.test - t(X.test)*b.PA) ^ 2)
-MSE.OLS <- mean((Y.test - t(X.test)*b.OLS) ^ 2)
-MSE.IV <- mean((Y.test - t(X.test)*b.IV) ^ 2)
-
-#Calculation ex2 figure 1
-#gamma.vec <- seq(0,10,by=0.01)
+##########################################################################
+# Calculation and plot of ex2 figure 1 in Rothenhaeusler 2020
 gamma.vec <- seq(0,100,by=0.1)
 b.vec <- numeric(length(gamma.vec))
 MSE.vec <- numeric(length(gamma.vec))
@@ -100,7 +89,7 @@ for (i in 1:length(gamma.vec)){
   b.vec[i] <- coef(fit)
 }
 
-# gamma limit for comparison to IV
+# gamma big enough to compare with IV
 gamma <- 10000
 fit <- anchor.regression(X.train, Y.train, A, gamma, n)
 MSE.limit <- mean((Y.test - t(X.test)*coef(fit)) ^ 2)
@@ -110,6 +99,12 @@ b.vec.limit <- c(b.vec,b.limit)
 MSE.vec.limit <- c(MSE.vec,MSE.limit)
 gamma.vec.limit <- c(gamma.vec,gamma)
 
+# test MSE for specific cases
+MSE.PA <- mean((Y.test - t(X.test)*b.PA) ^ 2)
+MSE.OLS <- mean((Y.test - t(X.test)*b.OLS) ^ 2)
+MSE.IV <- mean((Y.test - t(X.test)*b.IV) ^ 2)
+
+# plots
 plot(gamma.vec.limit, MSE.vec.limit, type = "l")
 points(1, MSE.OLS,col="2", pch=16)
 points(0, MSE.PA,col="4", pch=16)
@@ -132,15 +127,15 @@ points(b.PA, MSE.PA,col="4", pch=16)
 legend(1, 6.5, legend=c("OLS", "IV", "PA"),
        col=c(2, 3,4), cex=0.8,pch=16)
 
-# adding gamma axis
-P.A <- A%*%solve(t(A)%*%A)%*%t(A)
+# final plot: b and gamma vs test MSE
+P.A <- A%*%solve(t(A)%*%A)%*%t(A) # first calulate coresponding gammas
 X <- X.train
 Y <- Y.train
-
 b.gamma <- c(1,1.25,1.5,1.75,2)
 b.gamma <- seq(1,2,by=1/9)
 gamma.axis <- numeric(length(b.gamma))
 for (i in 1:length(b.gamma)) {
+  
   b <- b.gamma[i]
   
   gamma.axis[i] <- (-2*t(X)%*%X*b+t(X)%*%Y+t(Y)%*%X) * # solved by derivation of b optictive, set to 0 and solve for gamma
@@ -152,8 +147,8 @@ gamma.axis
 c("inf",17.6987,7.894467, 4.542962, 2.851117, 1.8308, 1.148405, 0.659905, 0.2929409, 0.007174545)
 
 library(ggplot2)
-data <- data.frame(b=b.vec.limit, gamma = gamma.vec.limit, trainMSE = MSE.vec.limit)
-ggplot(data, aes(y=trainMSE)) +
+data <- data.frame(b=b.vec.limit, gamma = gamma.vec.limit, testMSE = MSE.vec.limit)
+ggplot(data, aes(y=testMSE)) +
   
   geom_line( aes(x=b))+
   
@@ -175,3 +170,10 @@ ggplot(data, aes(y=trainMSE)) +
                         breaks=c(1.000000, 1.111111, 1.222222, 1.333333, 1.444444, 1.555556, 1.666667, 1.777778, 1.888889, 2.000000),
                         labels=c("inf",17.7,7.9, 4.5, 2.9, 1.8, 1.1, 0.7, 0.3, 0.0))
   )
+
+# unobserved optimal gamma
+b.opt <- b.vec.limit[which(MSE.vec.limit==min(MSE.vec.limit))]
+gamma.opt <- (-2*t(X)%*%X*b.opt+t(X)%*%Y+t(Y)%*%X) * # solved by derivation of b optictive, set to 0 and solve for gamma
+  solve(2*t(X)%*%P.A%*%X*b.opt-t(X)%*%P.A%*%Y-t(Y)%*%P.A%*%X)+1
+
+gamma.opt
