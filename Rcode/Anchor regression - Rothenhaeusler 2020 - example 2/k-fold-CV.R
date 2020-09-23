7b0dff747050a32d5ab3a0c4e4ef58c68f1f6262
rakittam <rakittam@student.ethz.ch>
2020-09-23 17:17:17 +0200

    theoretically there is no global best gamma, that is best for all unseen pertubations. The optimal gamma computed by CV uses only the information of the seen influence of A, (different approach upcoming: quantiles)

--- Rcode/Anchor regression - Rothenhaeusler 2020 - example 2/k-fold-CV.R
+++ Rcode/Anchor regression - Rothenhaeusler 2020 - example 2/k-fold-CV.R
@@ -61,7 +61,7 @@ MSE.OLS <- mean((Y.test - t(X.test)*b.OLS) ^ 2)
 MSE.IV <- mean((Y.test - t(X.test)*b.IV) ^ 2)
 
 ##########################################################################
-# k-fold Cross Validation 
+# k-fold Cross Validation
 
 data <- data.frame(Y=Y.train,X=X.train, A=A) # create data frame for CV
 
@@ -95,9 +95,10 @@ MSE.CV <- apply(MSE.CV.matrix,1,mean) # average over the k folds
 gamma.opt.CV <- gamma.CV[which(MSE.CV==min(MSE.CV))]
 gamma.opt.CV
 
-# plot CV test MSE
-plot(gamma.CV,MSE.CV)
-abline(v=gamma.opt.CV, col="forestgreen")
+fit.opt.CV <- anchor.regression(X.train, Y.train, A, gamma.opt.CV, n)
+
+b.opt.CV <- coef(fit.opt.CV)
+b.opt.CV
 
 ##########################################################################
 # Compare CV MSE to test MSE of pertubed data
@@ -125,5 +126,93 @@ plot(gamma.CV,MSE.CV, type = "l", ylim = c(min(min(MSE.CV),min(MSE.pert)),max(ma
 lines(gamma.CV,MSE.pert, col = 2, lty = "dashed")
 abline(v=gamma.opt.CV, col="forestgreen")
 abline(v=gamma.opt.pert, col="blue")
-legend(3.5, 6.5, legend=c("CV MSE", "true MSE", "CV optimal g", "true optimal g"),
+legend(1.7, 6.5, legend=c("CV MSE", "true MSE", "CV optimal g", "true optimal g"),
        col=c(1, 2, "forestgreen", "blue"), cex=0.8,pch=16)
+
+
+##########################################################################
+# Calculation and plot of ex2 figure 1 in Rothenhaeusler 2020
+gamma.vec <- seq(0,100,by=0.1)
+b.vec <- numeric(length(gamma.vec))
+MSE.vec <- numeric(length(gamma.vec))
+
+for (i in 1:length(gamma.vec)){
+  
+  gamma <- gamma.vec[i]
+  
+  fit <- anchor.regression(X.train, Y.train, A, gamma, n)
+  
+  MSE.vec[i] <- mean((Y.test - t(X.test)*coef(fit)) ^ 2)
+  b.vec[i] <- coef(fit)
+}
+
+# gamma big enough to compare with IV
+gamma <- 10000
+fit <- anchor.regression(X.train, Y.train, A, gamma, n)
+MSE.limit <- mean((Y.test - t(X.test)*coef(fit)) ^ 2)
+b.limit <- coef(fit)
+
+b.vec.limit <- c(b.vec,b.limit)
+MSE.vec.limit <- c(MSE.vec,MSE.limit)
+gamma.vec.limit <- c(gamma.vec,gamma)
+
+# test MSE for specific cases
+MSE.PA <- mean((Y.test - t(X.test)*b.PA) ^ 2)
+MSE.OLS <- mean((Y.test - t(X.test)*b.OLS) ^ 2)
+MSE.IV <- mean((Y.test - t(X.test)*b.IV) ^ 2)
+
+# gamma axis
+P.A <- A%*%solve(t(A)%*%A)%*%t(A) # first calulate coresponding gammas
+X <- X.train
+Y <- Y.train
+b.gamma <- c(1,1.25,1.5,1.75,2)
+b.gamma <- seq(1,2,by=1/9)
+gamma.axis <- numeric(length(b.gamma))
+for (i in 1:length(b.gamma)) {
+  
+  b <- b.gamma[i]
+  
+  gamma.axis[i] <- (-2*t(X)%*%X*b+t(X)%*%Y+t(Y)%*%X) * # solved by derivation of b optictive, set to 0 and solve for gamma
+    solve(2*t(X)%*%P.A%*%X*b-t(X)%*%P.A%*%Y-t(Y)%*%P.A%*%X)+1
+  
+}
+
+gamma.axis
+c("inf",17.6987,7.894467, 4.542962, 2.851117, 1.8308, 1.148405, 0.659905, 0.2929409, 0.007174545)
+
+# unobserved optimal gamma
+b.opt <- b.vec.limit[which(MSE.vec.limit==min(MSE.vec.limit))]
+gamma.opt <- (-2*t(X)%*%X*b.opt+t(X)%*%Y+t(Y)%*%X) * # solved by derivation of b optictive, set to 0 and solve for gamma
+  solve(2*t(X)%*%P.A%*%X*b.opt-t(X)%*%P.A%*%Y-t(Y)%*%P.A%*%X)+1
+
+gamma.opt
+
+library(ggplot2)
+data <- data.frame(b=b.vec.limit, gamma = gamma.vec.limit, testMSE = MSE.vec.limit)
+ggplot(data, aes(y=testMSE)) +
+  
+  geom_line( aes(x=b))+
+  
+  annotate("point", x = b.OLS, y = MSE.OLS, colour = 2)+
+  annotate("text", x = b.OLS-0.02, y = MSE.OLS+0.1, label = "OLS")+
+  annotate("point", x = b.IV, y = MSE.IV, colour = 3)+
+  annotate("text", x = b.IV, y = MSE.IV+0.1, label = "IV")+
+  annotate("point", x = b.PA, y = MSE.PA, colour = 4)+
+  annotate("text", x = b.PA-0.04, y = MSE.PA, label = "PA")+
+  
+  scale_x_continuous(
+    
+    # Features of the first axis
+    name = "b",
+    
+    # Add a second axis and specify its features
+    sec.axis = sec_axis(trans= ~.,
+                        name="gamma",
+                        breaks=c(1.000000, 1.111111, 1.222222, 1.333333, 1.444444, 1.555556, 1.666667, 1.777778, 1.888889, 2.000000),
+                        labels=c("inf",17.7,7.9, 4.5, 2.9, 1.8, 1.1, 0.7, 0.3, 0.0))
+  )+
+  geom_vline(xintercept = b.opt.CV, linetype="dotted", 
+             color = "blue", size=1)+
+  geom_vline(xintercept = b.opt, linetype="dotted", 
+             color = "forestgreen", size=1)
+
