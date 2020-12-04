f684e7e1d246ef4f8403a1e90e4f783093e23d03
rakittam <rakittam@student.ethz.ch>
2020-12-04 10:03:05 +0100

    studying simulation scenarios

--- R/1_run_simulations.R
+++ R/1_run_simulations.R
@@ -222,10 +222,12 @@ simulate_fivi <- function(nsim, nobs = 300, xi_values, xi_big = 10000,
 
 onerep_fixi <- function(rep, nobs = 300, data_table, data_pert_table, 
                         formula, A_formula, xi, xi_big = 10000,
-                        family, type, quant_value = 0.9) {
+                        family, type, quant_value = 0.9, states) {
+  
   
   # Generate data with nobs observations
   dd <- genData(nobs, data_table)
+  # .Random.seed <- states
   dd_pert <- genData(nobs, data_pert_table)
   
   # Fit GLM
@@ -293,7 +295,7 @@ simulate_fixi <- function(nsim, nobs = 300, xi, xi_big = 10000, v_values,
   
   v_len <- length(v_values)
   
-  states <- array(dim = c(nsim, v_len, 626))
+  states <- array(dim = c(nsim, 626))
   sim_data <- data.frame(matrix(nrow = nsim * v_len, ncol = 12))
   colnames(sim_data) <- c("v", "rep", "xi",
                           "b_glm", "se_glm", "logLik_glm_pert",
@@ -302,21 +304,21 @@ simulate_fixi <- function(nsim, nobs = 300, xi, xi_big = 10000, v_values,
  
   pb <- txtProgressBar(min = 0, max = nsim, style = 3)
   for (r in 1:nsim) {
+    states[r, ] <- .Random.seed
     for (s in 1:v_len) {
-      
-      states[r, s, ] <- .Random.seed
-      
       data_pert_table_temp <- updateDef(data_pert_table,
                                         changevar = "v",
                                         newformula = v_values[s])
-      
       sim_data[(v_len * (r - 1) + s), 1] <- v_values[s]
+      
+      # .Random.seed <- states[r, ]
+      
       sim_data[(v_len * (r - 1) + s), 2:12] <-
         onerep_fixi(rep = r, nobs = nobs,
-                    data_table = data_table, data_pert_table = data_pert_table, 
+                    data_table = data_table, data_pert_table = data_pert_table_temp, 
                     formula = formula, A_formula = A_formula,
                     xi = xi, xi_big = xi_big, family = family, type = type,
-                    quant_value = quant_value)
+                    quant_value = quant_value, states = states[r, ])
     }
     Sys.sleep(0.1)
     setTxtProgressBar(pb, r)
@@ -363,7 +365,7 @@ hist(dd$Y)
 set.seed(1813)
 
 # Simulate
-sim_data_rot <- simulate_rot(nsim = 10, nobs = 300,
+sim_data_rot <- simulate_rot(nsim = 10, nobs = 1000,
                              xi_values = seq(-0.5, 50, by = 0.5), xi_big = 10000,
                              data_table = def_rot, data_pert_table = def_rot_pert, 
                              formula = Y ~ X - 1, A_formula = ~ A - 1,
@@ -380,24 +382,141 @@ plot_rot(data_rot)
 # Anchor on X (IV setting) ----------------------------------------------------
 
 # Define variables for unperturbed and perturbed data set
+# def_nor_X <- defData(varname = "randA", dist = "binary",
+#                      formula = 0.5)
+# def_nor_X <- defData(def_nor_X, varname = "A", 
+#                      formula = "2 * randA - 1")
+def_nor_X <- defData(varname = "A", dist = "normal",
+                     formula = 0, variance = 0.01)
+def_nor_X <- defData(def_nor_X, varname = "H", dist = "normal",
+                     formula = 0, variance = 0.01)
+def_nor_X <- defData(def_nor_X, varname = "X", dist = "normal", 
+                     formula = "0.8 * H + A", variance = 0.01)
+def_nor_X <- defData(def_nor_X, varname = "Y", dist = "normal", 
+                     formula = "0.3 * X - 0.7 * H", variance = 0.25)
+
+# def_nor_X_pert <- defData(varname = "randA", dist = "binary",
+#                           formula = 0.5)
+# def_nor_X_pert <- defData(def_nor_X_pert, varname = "A", 
+#                           formula = "2 * randA - 1")
+def_nor_X_pert <- defData(varname = "A", dist = "normal",
+                          formula = 0, variance = 0.01)
+def_nor_X_pert <- defData(def_nor_X_pert, varname = "v", 
+                          formula = 3) # set perturbation strength
+def_nor_X_pert <- defData(def_nor_X_pert, varname = "H", dist = "normal",
+                          formula = 0, variance = 0.01)
+def_nor_X_pert <- defData(def_nor_X_pert, varname = "X", dist = "normal", 
+                          formula = "0.8 * H + v * A", variance = 0.01)
+def_nor_X_pert <- defData(def_nor_X_pert, varname = "Y", dist = "normal",
+                          formula = "0.3 * X - 0.7 * H", variance = 0.25)
+
+dd <- genData(300, def_nor_X)
+dd_pert <- genData(300, def_nor_X_pert)
+hist(dd$Y)
+
+# Initialize for fixed v
+set.seed(4981)
+nsim <- 10
+
+xi_values <- seq(0, 10, by = 0.1)
+xi_values <- xi_values[-1]
+xi_big <- 10000
+
+data_table <- def_nor_X
+data_pert_table <- def_nor_X_pert
+formula <- Y ~ X - 1
+A_formula <- ~ A - 1
+family <- gaussian
+type <- "pearson"
+
+# nobs <- 300
+# rep <- 1
+# data_table <- def_nor_X
+# data_pert_table <- def_nor_X_pert
+
+# Simulate
+sim_data_nor_X_fivi <- simulate_fivi(nsim = nsim, nobs = 300,
+                                     xi_values = xi_values, xi_big = xi_big,
+                                     data_table = data_table,
+                                     data_pert_table = data_pert_table, 
+                                     formula = formula, A_formula = A_formula,
+                                     family = family, type = type,
+                                     quant_value = 0.9) 
+
+data_nor_X_states_fivi <- sim_data_nor_X_fivi$states
+data_nor_X_fivi <- sim_data_nor_X_fivi$sim_data
+
+head(data_nor_X_fivi)
+summary(data_nor_X_fivi)
+
+plot_fivi(data_nor_X_fivi, xi_big = xi_big)
+
+# Initialize for fixed xi
+set.seed(3316)
+nsim <- 5
+
+xi <- 2
+xi_big <- 10000
+v_values <- seq(0, 100, by = 1)
+
+data_table <- def_nor_X
+data_pert_table <- def_nor_X_pert
+formula <- Y ~ X - 1
+A_formula <- ~ A - 1
+family <- gaussian
+type <- "pearson"
+
+# Simulate
+sim_data_nor_X_fixi <- simulate_fixi(nsim = nsim, nobs = 300,
+                                     xi = xi, xi_big = xi_big,
+                                     v_values = v_values, 
+                                     data_table = data_table,
+                                     data_pert_table = data_pert_table, 
+                                     formula = formula, A_formula = A_formula,
+                                     family = family, type = type,
+                                     quant_value = 0.9)
+
+data_nor_X_states_fixi <- sim_data_nor_X_fixi$states
+data_nor_X_fixi <- sim_data_nor_X_fixi$sim_data
+
+head(data_nor_X_fixi)
+summary(data_nor_X_fixi)
+
+# red = GLM, green = glare, blue = xi_big
+plot_fixi(data_nor_X_fixi)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Define variables for unperturbed and perturbed data set
 def_bin_X <- defData(varname = "A", dist = "normal", 
-                     formula = 0, variance = 0.25)
+                     formula = 0, variance = 0.01)
 def_bin_X <- defData(def_bin_X, varname = "H", dist = "normal",
-                     formula = 0, variance = 0.25)
+                     formula = 0, variance = 0.01)
 def_bin_X <- defData(def_bin_X, varname = "X", dist = "normal", 
-                     formula = "0.7 * H + A", variance = 0.25)
+                     formula = "0.7 * H + A", variance = 0.01)
 def_bin_X <- defData(def_bin_X, varname = "Y", dist = "binomial", link = "logit", 
                      formula = "-log(3) - 0.3 * X + H", variance = 1)
 
 def_bin_X_pert <- defData(varname = "A", dist = "normal", 
-                          formula = 0, variance = 0.25)
+                          formula = 0, variance = 0.01)
 def_bin_X_pert <- defData(def_bin_X_pert, varname = "v", 
-                          formula = 1.5) # set perturbation strength
+                          formula = 20) # set perturbation strength
 def_bin_X_pert <- defData(def_bin_X_pert, varname = "H", dist = "normal",
-                          formula = 0, variance = 0.25)
+                          formula = 0, variance = 0.01)
 def_bin_X_pert <- defData(def_bin_X_pert, varname = "X", dist = "normal", 
-                          formula = "0.7 * H + v * A", variance = 0.25)
-def_bin_X_pert <- defData(def_bin_X_pert, varname = "Y", dist = "binomial", link = "logit", 
+                          formula = "0.7 * H + v * A", variance = 0.01)
+def_bin_X_pert <- defData(def_bin_X_pert, varname = "Y", dist = "binomial", link = "logit",
                           formula = "-log(3) - 0.3 * X + H", variance = 1)
 
 dd <- genData(300, def_bin_X)
@@ -445,7 +564,7 @@ plot_fivi(data_bin_X_fivi, xi_big = xi_big)
 set.seed(3316)
 nsim <- 10
 
-xi <- 2
+xi <- 8
 xi_big <- 10000
 v_values <- seq(0, 100, by = 1)
 
