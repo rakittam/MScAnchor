d1dbd38e4ea3c033a840c07531ffab40f0fbb9c1
rakittam <rakittam@student.ethz.ch>
2021-02-08 18:04:19 +0100

    continued boston housing, used same covariates as lucas, leave-one-environment-out CV

--- R/data analysis/bostonHousing.R
+++ R/data analysis/bostonHousing.R
@@ -1,21 +1,26 @@
 # GLARE for Boston Housing Data
 #
-# Date: 04.01.21     Author: Maic Rakitta
+# Date: 08.02.21     Author: Maic Rakitta
 ###############################################################################
 
 library(glare)
 library(mlbench)
 library(ggplot2)
 library(tidyr)
+library(ggpubr) # for theme and ggarrange
+
+theme_set(theme_bw())
 
 # Load data
-data("BostonHousing2") # see help file why we use nr.2
+data("BostonHousing2")
 bostonPrices <- BostonHousing2[, c("cmedv", "crim", "zn", "indus", "nox",
                                    "rm", "age", "lstat", "town")]
 
 # Explore data ----------------------------------------------------------------
 plot(bostonPrices)
 summary(bostonPrices)
+
+table(bostonPrices$town)
 
 library(Hmisc)
 hist.data.frame(BostonHousing2[, c("cmedv", "crim", "zn", "indus", "nox",
@@ -45,19 +50,24 @@ hist(log(bostonPrices$lstat))
 # Correlation of continuous variables
 cor(bostonPrices[, c("cmedv", "crim", "zn", "indus", "nox",
                      "rm", "age", "lstat")], bostonPrices$cmedv)
-# -> lstat&ptratio high negative corr, rm high positive corr
+# -> lstat high negative corr, rm high positive corr
 
 bostonPrices$cmedv <- log(bostonPrices$cmedv)
 colnames(bostonPrices)[1] <- "log_cmedv"
 
 # Fit GLARE -------------------------------------------------------------------
 
-# Towns with 15 and more observations
-perturbation_towns <- c("Cambridge", "Boston Savin Hill", "Lynn",
-                        "Boston Roxbury", "Newton", "Somerville",
-                        "Boston South Boston", "Quincy", "Brookline",
-                        "Boston East Boston", "Waltham", "Medford",
-                        "Boston Dorchester", "Framingham")
+# Initialize
+xi_values <- c(0, 1, 5, 10, 50, 100, 10000)
+xi_len <- length(xi_values)
+
+bostonPrices$town <- factor(bostonPrices$town)
+names.arg <- levels(bostonPrices$town)
+levels(bostonPrices$town) <- abbreviate(names.arg, minlength = 2, use.classes = TRUE,
+                                        dot = FALSE, strict = FALSE,
+                                        method = c("left.kept", "both.sides"), named = TRUE)
+
+perturbation_towns <- names(table(bostonPrices$town))
 
 fit_list <- list()
 
@@ -70,11 +80,8 @@ for (t in 1:length(perturbation_towns)) {
   # Create observed and unobserved environment
   train_set <- bostonPrices[bostonPrices$town != pert_town, ]
   test_set <- bostonPrices[bostonPrices$town == pert_town, ]
-  
-  # Initialize and run GLARE
-  xi_values <- c(0, 1, 5, 10, 50, 100, 10000)
-  xi_len <- length(xi_values)
-  
+
+  # Run GLAREs
   b <- matrix(nrow = xi_len, ncol = 8)
   colnames(b) <- c("Intercept", "crim", "zn", "indus",
                    "nox", "rm", "age","lstat")
@@ -84,9 +91,6 @@ for (t in 1:length(perturbation_towns)) {
   predictions <- matrix(nrow = xi_len, ncol = nrow(test_set))
   resid_pert <- matrix(nrow = xi_len, ncol = nrow(test_set))
   RMSE <- numeric(xi_len)
-  
-  logLik_indiv <- matrix(nrow = xi_len, ncol = nrow(test_set))
-  logLik <- numeric(xi_len)
   
   pb_xi <- txtProgressBar(min = 0, max = xi_len, style = 3)
   for (i in 1:xi_len) {
@@ -107,32 +111,34 @@ for (t in 1:length(perturbation_towns)) {
     resid_pert[i, ] <- test_set$log_cmedv - predictions[i, ]
     RMSE[i] <- sqrt(mean((resid_pert[i, ])^2))
     
-    # Log-likelihood
-    logLik_indiv[i, ] <- as.numeric(logLik(fit_temp, newdata = test_set,
-                                           indiv = TRUE))
-    logLik[i] <- as.numeric(quantile(logLik_indiv[i, ], 0.9))
-    
     Sys.sleep(0.1)
     setTxtProgressBar(pb_xi, i)
   }
   close(pb_xi)
   
-  fit_list[[t]] <- list(b, b_se, predictions, RMSE, logLik_indiv, logLik)
+  fit_list[[t]] <- list(b, b_se, predictions, RMSE)
 }
 
-# path_name <- "C:/Users/maicr/Desktop/Github/MScAnchor/data sets/data_examples/bostonHousing/"
+# path_name <- "./data sets/data_examples/bostonHousing/"
 # dir.create(paste(path_name, Sys.Date(), sep = ""))
-# save(fit_list, file = paste(paste(path_name, Sys.Date(), sep = ""), "/fit_list_lucas_covar.Rdata", sep =""))
+# save(fit_list, file = paste(paste(path_name, Sys.Date(), sep = ""), "/fit_list.Rdata", sep =""))
 
 # Plots -----------------------------------------------------------------------
 
 # Prepare data
-xi_values <- c(0, 1, 5, 10, 50, 100, 10000)
-xi_len <- length(xi_values)
-# load("C:/Users/maicr/Desktop/Github/MScAnchor/data sets/data_examples/bostonHousing/2021-01-07/fit_list.Rdata")
+# xi_values <- c(0, 1, 5, 10, 50, 100, 10000)
+# xi_len <- length(xi_values)
+# load("./data sets/data_examples/bostonHousing/2021-02-08/fit_list.Rdata")
 
-gg_data <- data.frame(matrix(ncol = 3, nrow = 0))
-colnames(gg_data) <- c("test_town", "xi", "RMSE")
+gg_data <- data.frame(matrix(ncol = 4, nrow = 0))
+colnames(gg_data) <- c("test_town", "xi", "RMSE", "b")
+
+b_matrix <- matrix(0, nrow = 0, ncol = 10)
+colnames(b_matrix) <- c("xi", "Town", "Intercept", "crim", "zn", "indus",
+                 "nox", "rm", "age","lstat")
+b_matrix <- data.frame(b_matrix)
+b_matrix <- transform(b_matrix, xi = as.character(xi))
+b_matrix <- transform(b_matrix, Town = as.character(Town))
 
 for (t in 1:length(perturbation_towns)) {
   
@@ -143,20 +149,19 @@ for (t in 1:length(perturbation_towns)) {
     xi <- xi_values[i]
     
     RMSE <- fit_list[[t]][[4]][i]
+    b <- fit_list[[t]][[1]][i, ]
   
-    data_temp <- data.frame(test_town, xi, RMSE)
+    data_temp <- data.frame(test_town, xi, RMSE, b)
     gg_data <- rbind(gg_data, data_temp)
+    
+    b_frame <- data.frame(t(b))
+    b_frame <- cbind(xi, test_town, b_frame)
+    b_matrix <- rbind(b_matrix, b_frame)
   }
 }
 
 # Plot all
-gg_data$xi <- factor(gg_data$xi, levels = c("0", "1", "5", "10", "50", "100", "10000"))
-
-gg_data$test_town <- factor(gg_data$test_town)
-names.arg <- levels(gg_data$test_town)
-levels(gg_data$test_town) <- abbreviate(names.arg, minlength = 2, use.classes = TRUE,
-                                        dot = FALSE, strict = FALSE,
-                                        method = c("left.kept", "both.sides"), named = TRUE)
+gg_data$xi <- factor(gg_data$xi, levels = c(0, 1, 5, 10, 50, 100, 10000))
 
 ggplot(gg_data, aes(y = RMSE, x = test_town, color = xi, group = xi)) +
   
@@ -164,210 +169,101 @@ ggplot(gg_data, aes(y = RMSE, x = test_town, color = xi, group = xi)) +
   
   labs(color = expression(xi)) +
   ylab("RMSE") +
-  xlab("Perturbation town")
+  xlab("Perturbation town") +
+  scale_color_viridis_d(option = "C", end = 0.95)
 
 # Plot 0, 5 , 50, 10000
 gg_data2 <- gg_data[gg_data$xi %in% c(0,5,50,10000), ]
 
-ggplot(gg_data2, aes(y = RMSE, x = test_town, color = xi, group = xi)) +
+gg <- ggplot(gg_data2, aes(y = RMSE, x = test_town, color = xi, group = xi)) +
   
   geom_line() +
   
   labs(color = expression(xi)) +
   ylab("RMSE") +
-  xlab("Perturbation town")
+  xlab("Perturbation town") +
+  scale_color_viridis_d(option = "C", end = 0.95)
 
-# citys with 50 as best:
-# somerville, Boston East Boston, Medford, Boston Dorchester, Newton
+ggsave("all.pdf", gg, width = 30, height = 9)
 
-# citys with xi_big as best:
-# Boston South Boston, Brookline
+# Only certain towns
+gg_data_towns <- gg_data2[gg_data2$test_town %in% c("Ar", "As", "Bd",
+                                                    "Bl", "Bv", "BA",
+                                                    "BBB", "BBH", "BC",
+                                                    "BstnDr", "BstnDw", "BEB",
+                                                    "BFH", "BHP", "BM",
+                                                    "BNE"), ]
 
-# citys with 5 as best:
-# Chambridge
-
-# rest best with 0
-
-
-
-
-
-
-
-
-
-# Create observed and unobserved environment
-train_set <- bostonPrices[bostonPrices$town != "Medford", ]
-test_set <- bostonPrices[bostonPrices$town == "Medford", ]
-
-nrow(train_set)
-nrow(test_set)
-
-# Initialize and run GLARE
-xi_values <- seq(0, 100, by = 1)
-
-#xi_values <- c(0, 5, 10, 20, 50, 100, 200, 400, 700, 1000)
-
-xi_len <- length(xi_values)
-
-b <- matrix(nrow = xi_len, ncol = 8)
-colnames(b) <- c("Intercept", "crim", "zn", "indus",
-                 "nox", "rm", "age","lstat")
-b_se <- matrix(nrow = xi_len, ncol = 8)
-colnames(b_se) <- colnames(b)
-
-predictions <- matrix(nrow = xi_len, ncol = nrow(test_set))
-resid_pert <- matrix(nrow = xi_len, ncol = nrow(test_set))
-RMSE <- numeric(xi_len)
-
-logLik_indiv <- matrix(nrow = xi_len, ncol = nrow(test_set))
-logLik <- numeric(xi_len)
-
-pb <- txtProgressBar(min = 0, max = xi_len, style = 3)
-for (i in 1:xi_len) {
+ggplot(gg_data_towns, aes(y = RMSE, x = test_town, color = xi, group = xi)) +
   
-  xi <- xi_values[i]
-
-  fit_temp <- glare(formula = log_cmedv ~ crim + zn + indus + nox + rm +
-                      age + lstat,
-                    A_formula = ~ town, data = train_set, xi = xi,
-                    family = gaussian, type = "pearson")
-  # 
-  # fit_temp <- glare(formula = cmedv ~ crim + zn + indus + chas + nox + rm + age +
-  #                     dis + rad + tax + ptratio + b + lstat,
-  #                   A_formula = ~ town, data = train_set, xi = xi,
-  #                   family = gaussian, type = "pearson")
+  geom_line() +
   
-  # resid_pert <- test_set$cmedv - predict(fit_temp,
-  #                                            type = "response",
-  #                                            newdata = test_set)
+  labs(color = expression(xi)) +
+  ylab("RMSE") +
+  xlab("Perturbation town") +
+  scale_color_viridis_d(option = "C", end = 0.95)
+
+# Upper towns
+upper_RMSE <- sort(gg_data$RMSE, decreasing = TRUE)[1:500]
+upper_index <- which(gg_data$RMSE %in% upper_RMSE)
+upper_table <- table(gg_data$test_town[upper_index])
+upper_table_t <- upper_table[upper_table != 0]
+upper_towns <- names(upper_table_t)
+
+gg_data_upper <- gg_data2[gg_data2$test_town %in% upper_towns, ]
+
+gg_upper <- ggplot(gg_data_upper, aes(y = RMSE, x = test_town, color = xi, group = xi)) +
   
-  # Coefficients
-  b[i, ] <- as.numeric(coef(fit_temp))
-  b_se[i, ] <- fit_temp$coef_se
+  geom_line() +
   
-  # Predictions, Residuals and MSE
-  predictions[i, ] <- predict(fit_temp, type = "response", newdata = test_set)
-  resid_pert[i, ] <- test_set$log_cmedv - predictions[i, ]
-  RMSE[i] <- sqrt(mean((resid_pert[i, ])^2))
+  labs(color = expression(xi)) +
+  ylab("RMSE") +
+  xlab("Perturbation town") +
+  scale_color_viridis_d(option = "C", end = 0.95)
+
+ggsave("upper.pdf", gg_upper, width = 9, height = 9)
+
+# Towns with more then 10 obs
+table_10 <- table(bostonPrices$town)[table(bostonPrices$town) >= 10]
+towns_10 <- names(table_10)
+
+gg_data_10 <- gg_data2[gg_data2$test_town %in% towns_10, ]
+
+gg_10 <- ggplot(gg_data_10, aes(y = RMSE, x = test_town, color = xi, group = xi)) +
   
-  # Log-likelihood
-  logLik_indiv[i, ] <- as.numeric(logLik(fit_temp, newdata = test_set,
-                                         indiv = TRUE))
-  logLik[i] <- as.numeric(quantile(logLik_indiv[i, ], 0.9))
+  geom_line() +
   
-  Sys.sleep(0.1)
-  setTxtProgressBar(pb, i)
-}
-close(pb)
+  labs(color = expression(xi)) +
+  ylab("RMSE") +
+  xlab("Perturbation town") +
+  scale_color_viridis_d(option = "C", end = 0.95)
 
-# list <- list(b, b_se, predictions, RMSE, logLik_indiv, logLik)
-# path_name <- "C:/Users/maicr/Desktop/Github/MScAnchor/data sets/data_examples/bostonHousing/"
-# dir.create(paste(path_name, Sys.Date(), sep = ""))
-# save(list, file = paste(paste(path_name, Sys.Date(), sep = ""), "/list_Medford_all.Rdata", sep =""))
+gg_10
 
-#load("C:/Users/maicr/Desktop/Github/MScAnchor/data sets/data_examples/bostonHousing/2021-01-11/list_Medford_all.Rdata")
+ggsave("10obs.pdf", gg_10, width = 9, height = 9)
 
-b <- list[[1]]
-
-
-# Fit GLARE for xi big
-xi_big <- 10000
-fit_big <- glare(formula = log_cmedv ~ crim + zn + indus + nox + rm +
-                    age + lstat,
-                  A_formula = ~ town, data = train_set, xi = xi_big,
-                  family = gaussian, type = "pearson")
-b_big <- as.numeric(coef(fit_big))
-b_se_big <- fit_big$coef_se
-predictions_big <- predict(fit_big, type = "response", newdata = test_set)
-resid_pert_big <- test_set$log_cmedv - predictions_big
-RMSE_big <- sqrt(mean((resid_pert_big)^2))
-
-b_big
-RMSE_big
-
-
-
-# Plots -----------------------------------------------------------------------
-
+# -----------------------------------------------------------------------------
 # Parameter plot
-gg_data_b <- as.data.frame(cbind(xi_values, b))
+gg_b_data <- b_matrix
+gg_b_data$xi <- factor(gg_b_data$xi, levels = c(0, 1, 5, 10, 50, 100, 10000))
+gg_b_data$test_town <- factor(gg_b_data$test_town)
+names.arg <- levels(gg_b_data$test_town)
+levels(gg_b_data$test_town) <- abbreviate(names.arg, minlength = 2, use.classes = TRUE,
+                                        dot = FALSE, strict = FALSE,
+                                        method = c("left.kept", "both.sides"), named = TRUE)
 
-library(dplyr)
-df_b <- gg_data_b %>%
-  select(xi_values, Intercept, crim, zn, indus, nox, rm,
-         age, lstat) %>%
-  gather(key = "Variable", value = "value", -xi_values)
-head(df_b)
 
-ggplot(df_b, aes(x = xi_values, y = value)) + 
-  geom_line(aes(color = Variable)) +
-  ylab("Parameters") +
-  xlab(expression(xi)) +
-  geom_vline(xintercept = 0, linetype = "dashed")
+gg_b_data2 <- gg_b_data[gg_b_data$xi %in% c(0,5,50,10000), ]
 
-# 0.9-quantile of log-likelihood plot
-gg_data_log <- data.frame(logLik_pert = logLik, xi = xi_values)
-
-ggplot(gg_data_log, aes(y = logLik_pert, x = xi)) +
+ggplot(gg_b_data2, aes(y = indus, x = test_town, color = xi, group = xi)) +
+  
   geom_line() +
-  ylab("0.9-quantile of log-likelihood") +
-  xlab(expression(xi)) +
-  geom_vline(xintercept = 0, linetype = "dashed")
-
-# MSE
-gg_data <- data.frame(RMSE = RMSE, xi = xi_values)
-
-ggplot(gg_data, aes(y = RMSE, x = xi)) +
-  geom_line() +
-  xlab(expression(xi))
-
-
-gg_data2 <- gg_data[gg_data$xi <101,]
-
-ggplot(gg_data2, aes(y = RMSE, x = xi)) +
-  geom_line() +
-  xlab(expression(xi))
+  
+  labs(color = expression(xi)) +
+  xlab("Perturbation town") +
+  scale_color_viridis_d(option = "C", end = 0.95)
 
 
 
 
-# Single comparison -----------------------------------------------------------
 
-# Create observed and unobserved environment
-train_set <- bostonPrices[bostonPrices$town != "Medford", ]
-test_set <- bostonPrices[bostonPrices$town == "Medford", ]
-
-# Fit GLARE for xi fixed
-xi <- 5
-fit_glare <- glare(formula = log_cmedv ~ crim + zn + indus + nox + rm +
-                     age + lstat,
-                   A_formula = ~ town, data = train_set, xi = xi,
-                   family = gaussian, type = "pearson")
-summary(fit_glare)
-
-
-b_big <- as.numeric(coef(fit_big))
-b_se_big <- fit_big$coef_se
-predictions_big <- predict(fit_big, type = "response", newdata = test_set)
-resid_pert_big <- test_set$log_cmedv - predictions_big
-RMSE_big <- sqrt(mean((resid_pert_big)^2))
-
-b_big
-RMSE_big
-
-# Fit GLM
-fit_glm <- glm(formula = log_cmedv ~ crim + zn + indus + nox + rm +
-                 age + lstat,
-               data = train_set, family = gaussian)
-summary(fit_glm)
-
-# no age
-fit_glm <- glm(formula = log_cmedv ~ crim + zn + indus + nox + rm + lstat,
-               data = train_set, family = gaussian)
-summary(fit_glm)
-
-# no zn
-fit_glm <- glm(formula = log_cmedv ~ crim + nox + rm + lstat,
-               data = train_set, family = gaussian)
-summary(fit_glm)
